adb457ab41000a4431af0fabce5f1a61acb9f987
Steve Olsen <stevelolsen@gmail.com>
2017-08-18 14:27:47 -0700

    Finds the correct film and returns it

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = proces
 // START SERVER
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
+  .then(() => sqlite.open('./db/database.db'))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
 // ROUTES
@@ -16,7 +17,20 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+  let filmId = +req.params.id,
+      limit = 0,
+      offset = 0;
+
+  if (typeof filmId === 'number') {
+    sqlite.get('SELECT * FROM films WHERE id = ?', filmId)
+     		.then(film => {
+          res.status(200).send(film);
+        })
+        .catch(err => res.status(404).send(err));
+  }
+  else {
+    res.status(500).send('Not a film');
+  }
 }
 
 module.exports = app;
